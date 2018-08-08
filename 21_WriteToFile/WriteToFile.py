dde337b20d020163060f78e0744d67576f5f7d27
Jonathan Chan <Jonhschan@hotmail.com>
2018-08-07 20:00:00 -0400

    21

---
+++ 21_WriteToFile/WriteToFile.py
@@ -0,0 +1,16 @@
+# write to a txt file
+
+from DecodeWebPage import DECODE
+
+def WriteToFile():
+
+    DEC = DECODE()
+    retList = DEC.DecodeWebPage()
+
+    f = open('C:\\Users\\Jonathan\\Desktop\\test1\\21_WriteToFile\\textfile.txt','w')
+
+    for elem in retList:
+        f.write(str(elem))
+        f.write("\n")
+
+WriteToFile()
